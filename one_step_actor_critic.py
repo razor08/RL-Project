c38efd2ba73aa7e9c2a03d089e32e8ac9c0acaa9
Jay Sinha <jay.x.sinha@gmail.com>
2023-11-28 18:47:24 -0500

    Update one_step_actor_critic.py

--- one_step_actor_critic.py
+++ one_step_actor_critic.py
@@ -6,7 +6,7 @@ from cs687_gridworld import print_results, states, states_to_id, terminal_states
 from cartpole import Env as CartpoleEnv, actions as cartpole_actions
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+# import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
 
@@ -37,12 +37,8 @@ SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 class ActorCritic(nn.Module):
     def __init__(self, num_states, num_actions, hidden_dim=128):
         super(ActorCritic, self).__init__()
-        self.affine1 = nn.Linear(num_states, hidden_dim)
-
-        # actor's layer
+        self.common_layer = nn.Linear(num_states, hidden_dim)
         self.action_head = nn.Linear(hidden_dim, num_actions)
-
-        # critic's layer
         self.value_head = nn.Linear(hidden_dim, 1)
 
         # action & reward buffer
@@ -50,8 +46,8 @@ class ActorCritic(nn.Module):
         self.rewards = []
 
     def forward(self, x):
-        x = F.leaky_relu(self.affine1(x))
-        action_prob = F.softmax(self.action_head(x), dim=-1)
+        x = nn.functional.leaky_relu(self.common_layer(x))
+        action_prob = nn.functional.softmax(self.action_head(x), dim=-1)
         state_values = self.value_head(x)
         return action_prob, state_values
 
@@ -71,15 +67,9 @@ def select_action(state):
     else:
         state_tensor = torch.tensor(state, dtype = torch.float32)
         state_tensor = state_tensor.view(4)
-    try:
-      probs, state_value = model(state_tensor)
-      action_space = Categorical(probs)
-    except:
-      print(state)
-      print(state_tensor)
-      print(probs)
-      print(state_value)
-      raise Exception("Error Occurred")
+    
+    probs, state_value = model(state_tensor)
+    action_space = Categorical(probs)
     action = action_space.sample()
     model.saved_actions.append(SavedAction(action_space.log_prob(action), state_value))
     return action.item(), state_value
@@ -105,8 +95,8 @@ def finish_episode():
     for (log_prob, value), R in zip(saved_actions, returns):
         advantage = R - value.item()
         actor_losses.append(-log_prob * advantage)
-        critic_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
-        # critic_losses.append(F.mse_loss(value, torch.tensor([R])))
+        critic_losses.append(nn.functional.smooth_l1_loss(value, torch.tensor([R])))
+        # critic_losses.append(nn.functional.mse_loss(value, torch.tensor([R])))
 
     loss = torch.stack(actor_losses).sum() + torch.stack(critic_losses).sum()
     optimizer.zero_grad()
@@ -118,7 +108,7 @@ def finish_episode():
 
 running_reward = 0
 
-for iter in range(num_episodes):
+for i in range(num_episodes):
     
     state = env.reset()
     reward_episode = 0
@@ -136,9 +126,9 @@ for iter in range(num_episodes):
 
     running_reward = 0.05 * reward_episode + (1 - 0.05) * running_reward
     finish_episode()
-    if iter % print_episode == 0:
+    if i % print_episode == 0:
         print('Episode {}\tLast reward: {:.2f}\tRunning reward: {:.2f}'.format(
-                iter, reward_episode, running_reward))
+                i, reward_episode, running_reward))
     if running_reward >= env.spec['reward_threshold']:
         print("Solved! Running reward is now {:.2f} and "
                 "the last episode runs to {} time steps!".format(running_reward, t))
