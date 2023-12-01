fa963e3a3b59e547c47e17dcb2955209493609ee
Jay Sinha <jay.x.sinha@gmail.com>
2023-11-30 19:36:16 -0500

    Hopefully final push lol

--- reinforce_baseline.py
+++ reinforce_baseline.py
@@ -5,6 +5,7 @@ from torch.distributions import Categorical
 from cs687_gridworld import Env as GWEnv, actions as gridworld_actions
 from cs687_gridworld import print_results, states, states_to_id, terminal_states, wall_states
 from cartpole import Env as CartpoleEnv, actions as cartpole_actions
+import matplotlib.pyplot as plt
 # torch.autograd.set_detect_anomaly(True)
 
 num_episodes = 5000
@@ -45,8 +46,10 @@ class ReinforceBaseline(torch.nn.Module):
 
 
 model = ReinforceBaseline(num_states, num_actions, actor_critic_hidden_dim)
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-2)
-# optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# 3e-2 = 1104
+policy_optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
+
+value_optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
 eps = np.finfo(np.float32).eps.item()
 
 
@@ -68,8 +71,8 @@ def select_action(state):
 
 def train(values, rewards, log_probs):
     R = 0
-    actor_losses = []
-    critic_losses = []
+    policy_losses = []
+    value_losses = []
     returns = []
 
     for r in rewards[::-1]:
@@ -81,17 +84,23 @@ def train(values, rewards, log_probs):
 
     for log_prob, value, R in zip(log_probs, values, returns):
         advantage = R - value.item()
-        actor_losses.append(-log_prob * advantage)
-        critic_losses.append(torch.nn.functional.smooth_l1_loss(value, torch.tensor([R])))
-        loss = torch.stack(actor_losses).sum() + torch.stack(critic_losses).sum()
+        policy_losses.append(-log_prob * advantage)
+        value_losses.append(torch.nn.functional.smooth_l1_loss(value, torch.tensor([R])))
+        policy_loss = torch.stack(policy_losses).sum() 
+        values_loss = torch.stack(value_losses).sum()
 
-    optimizer.zero_grad()
-    loss.backward(retain_graph=True)
-    optimizer.step()
+    policy_optimizer.zero_grad()
+    policy_loss.backward(retain_graph=True)
+    policy_optimizer.step()
+
+    value_optimizer.zero_grad()
+    values_loss.backward(retain_graph=True)
+    value_optimizer.step()
 
 
 running_avg_reward = 0
-
+ra_tracker = []
+ra_std = []
 for i in range(1, num_episodes + 1):
     state = env.reset()
     reward_episode = 0
@@ -111,15 +120,28 @@ for i in range(1, num_episodes + 1):
         
     train(values, rewards, log_probs)
     running_avg_reward = 0.05 * reward_episode + 0.95 * running_avg_reward
+    ra_tracker.append(running_avg_reward)
+    ra_std.append(np.std(ra_tracker))
     if i % 25 == 0 and verbose:
         print(f'Ran till Episode: {i}\tLast Episode Reward: {reward_episode}\tRunning Average Reward: {running_avg_reward}')
     if running_avg_reward >= env.spec['reward_threshold']:
         print(f"Optimized the MDP in {i} episodes! Running Average Reward: {running_avg_reward}!")
         break
 
-num_episodes = 5
-model = model.eval()
+plt.figure(figsize=(8, 6))
+plt.plot(ra_tracker, label='Running Average')
+plt.fill_between(np.arange(len(ra_tracker)), np.array(ra_tracker) - np.array(ra_std), 
+                 np.array(ra_tracker) + np.array(ra_std), color='lightblue', alpha=0.3, label='Standard Deviation')
+plt.xlabel('Number of Episodes')
+plt.ylabel('Running Average Reward')
+plt.title('Running Average Rewards with Standard Deviation during Training')
+plt.legend()
+plt.grid(True)
+plt.show()
 
+total_rewards = []
+num_episodes = 50
+model = model.eval()
 for episode in range(1, num_episodes + 1):
     state = env.reset()
     total_reward = 0
@@ -133,7 +155,7 @@ for episode in range(1, num_episodes + 1):
 
         if done:
             break
-
+    total_rewards.append(total_reward)
     if environment == 'Gridworld':
         values = {}
         policy = {}
@@ -152,3 +174,15 @@ for episode in range(1, num_episodes + 1):
         print_results(values, policy)
     print(f"Episode {episode}: Total Reward: {total_reward:.2f}")
 
+total_std = np.std(total_rewards)
+print("Mean: ", np.mean(total_rewards))
+plt.figure(figsize=(8, 6))
+plt.plot(total_rewards, label='Running Average')
+plt.fill_between(np.arange(len(total_rewards)), np.array(total_rewards) - np.array(total_std), 
+                 np.array(total_rewards) + np.array(total_std), color='lightblue', alpha=0.3, label='Standard Deviation')
+plt.xlabel('Number of Episodes')
+plt.ylabel('Running Average Reward')
+plt.title('Running Average Rewards with Standard Deviation')
+plt.legend()
+plt.grid(True)
+plt.show()
